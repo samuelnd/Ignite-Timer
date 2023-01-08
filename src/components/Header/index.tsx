43f8b22e86098aab221d934be4fb5399e222ca42
samuel <samuelsnds09@outlook.com>
2023-01-08 13:29:47 -0300

    created validate form

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
-import { HeaderContainer } from "./styles";
+import { HeaderContainer } from "./styles"
+
+import logoIgnite from '../../assets/Logo.svg'
+import { Timer, Scroll } from "phosphor-react"
+import { NavLink } from "react-router-dom"
 
 export function Header() {
     return (
         <HeaderContainer>
-            <span>logo</span>
+            <img src={logoIgnite} alt="" />
             <nav>
-                <a href="">timer</a>
-                <a href="">history</a>
+                <NavLink to="/" title="Timer">
+                    <Timer size={24}/>
+                </NavLink>
+                <NavLink to="/history" title="Histórico">
+                    <Scroll size={24}/>
+                </NavLink>
             </nav>
         </HeaderContainer>
         )
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,88 @@
+import { Play } from "phosphor-react";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as zod from 'zod';
+
+import { 
+    HomeContainer, 
+    FormContainer, 
+    CountdownContainer, 
+    Separator, 
+    StartCountDownButton, 
+    TaskInput, 
+    MinutesAmountInput 
+} from "./styles"
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod.number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+export function Home() {
+    const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues:{
+            task: '',
+            minutesAmount: 0,
+        }
+    });
+
+    function handleCreateNewCycle(data: NewCycleFormData){
+        console.log(data)
+    }
+
+    const task = watch('task');
+    const isSubmitDisabled = !task;
+
+    return (
+        <HomeContainer>
+            <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <TaskInput 
+                    id="task" 
+                    list="task-suggestions" 
+                    placeholder="Dê um nome para seu projeto "
+                    {...register('task')}
+                    />
+
+                    <datalist id="task-suggestions">
+                        <option value="Projeto 1"></option>
+                        <option value="Projeto 2"></option>
+                        <option value="Projeto 3"></option>
+                        <option value="Banana"></option>
+                    </datalist>
+
+                    <label htmlFor="minutesAmount">durante</label>
+                    <MinutesAmountInput 
+                    type="number" 
+                    id="minutesAmount" 
+                    step={5}
+                    min={5}
+                    max={60}
+                    />
+
+                    <span>minutos.</span>
+                </FormContainer>
+                
+
+                <CountdownContainer>
+                    <span>0</span>
+                    <span>0</span>
+                    <Separator>:</Separator>
+                    <span>0</span>
+                    <span>0</span>
+                </CountdownContainer>
+
+                <StartCountDownButton disabled={isSubmitDisabled} type="submit">
+                    <Play size={24}/>
+                    Começar
+                </StartCountDownButton>
+            </form>
+        </HomeContainer>
+        )
+}
