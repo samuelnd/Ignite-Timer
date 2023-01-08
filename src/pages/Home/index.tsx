efb0ffebbb616bf6f9f6b31a18a385ce69836fc9
samuel <samuelsnds09@outlook.com>
2023-01-08 13:43:45 -0300

    update form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,7 +23,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-    const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+    const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
         defaultValues:{
             task: '',
@@ -32,7 +32,8 @@ export function Home() {
     });
 
     function handleCreateNewCycle(data: NewCycleFormData){
-        console.log(data)
+        console.log('oi')
+        reset();
     }
 
     const task = watch('task');
@@ -64,6 +65,7 @@ export function Home() {
                     step={5}
                     min={5}
                     max={60}
+                    {...register('minutesAmount', {valueAsNumber: true})}
                     />
 
                     <span>minutos.</span>
