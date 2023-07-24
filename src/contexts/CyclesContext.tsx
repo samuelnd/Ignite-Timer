ad027477134484faa031950a91a2152cd1a8b4bd
Samuel Nunes <samuelsnds09@outlook.com>
2023-07-23 23:06:40 -0300

    Reducers - Salvando estado no storage

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,7 +1,8 @@
-import { ReactNode, createContext, useState, useReducer } from "react";
+import { ReactNode, createContext, useState, useReducer, useEffect } from "react";
 
 import { Cycle, cyclesReducer} from "../reducers/cycles/reducer"
-import { ActionTypes, addNewCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
+import { addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
+import { differenceInSeconds } from "date-fns";
 
 interface CreateCycleData {
     task: string
@@ -28,13 +29,37 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
     const [ cyclesState, dispatch ] = useReducer(cyclesReducer, {
         cycles: [],
         activeCycleId: null
-    })
+    },
+     (initialState) => {
+        const storedStateAsJson = localStorage.getItem(
+            '@ignite-timer:cycles-state-1.0.0'
+        )
 
+        if(storedStateAsJson) {
+            return JSON.parse(storedStateAsJson)
+        }
 
-    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+        return initialState
+    },)
 
     const { cycles, activeCycleId } = cyclesState;
     const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+        if(activeCycle) {
+            return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
+        }
+
+        return 0
+    });
+
+    useEffect(() => {
+        const stateJSON = JSON.stringify(cyclesState)
+
+        localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON)
+    }, [cyclesState])
+
+    
 
     function setSecondsPassed(seconds: number) {
         setAmountSecondsPassed(seconds)
@@ -62,12 +87,7 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
 
     function interruptCurrentCycle() {
 
-        dispatch({
-            type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-            payload: {
-                activeCycleId
-            },
-        })
+        dispatch(interruptCurrentCycleAction())
 
     }
     
